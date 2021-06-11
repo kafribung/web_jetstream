1b17af9904a96185bf583c06c360f660cb59b260
=Kafriansyah <60900117001@uin-alauddin.ac.id>
2021-06-11 11:50:31 +0800

    clean code

--- config/fortify.php
+++ config/fortify.php
@@ -132,7 +132,7 @@ return [
     */
 
     'features' => [
-        Features::registration(),
+        // Features::registration(),
         Features::resetPasswords(),
         // Features::emailVerification(),
         Features::updateProfileInformation(),
@@ -141,5 +141,4 @@ return [
             'confirmPassword' => true,
         ]),
     ],
-
 ];
