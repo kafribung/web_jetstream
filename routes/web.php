f9b5978dc5ba529ea67da3b976713d2c9429d837
kafribung <60900117001@uin-alauddin.ac.id>
2021-06-13 10:55:11 +0800

    Add model category

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use  App\View\Components\AppLayout;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,11 +18,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
 
-// Route::middleware(['auth', 'verified'])->group(function(){
+Route::middleware(['auth', 'verified'])->group(function(){
+
+    // Dashboard
+    Route::get('dashboard', function(){
+        return view('dashboard');
+    })->name('dashboard');
+
+    // Category
     
-
-// });
+});
